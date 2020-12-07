207e5d7d4217a22219b6db67373bb214e93fa043
Jake Butler <tjakebutler@gmail.com>
2020-12-07 15:06:05 -0600

    created login and register components that are linked to my database, now users are able to login or register

---
+++ src/Sweat.js
@@ -0,0 +1,28 @@
+import React from "react"
+import {Route, Redirect} from "react-router-dom"
+import { Login } from "./components/auth/Login"
+import { Register } from "./components/auth/Register"
+
+
+
+
+export const Sweat = () => (
+    <>
+      <Route render={() => {
+          // The user id is saved under the key app_user_id in local Storage. Change below if needed!
+            if (localStorage.getItem("app_user_id")) {
+                return (
+                    <>
+                        //Components that are rendered when the user is authenticated go inside this React fragment
+                    </>
+                )
+            } else {
+                return <Redirect to="/login" />
+            }
+        }} />
+
+        <Route path="/login" render={props => <Login {...props} />} />
+        <Route path="/register" render={props => <Register {...props} />} />
+    </>
+    
+  )
