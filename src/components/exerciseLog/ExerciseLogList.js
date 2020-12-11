cf1ed42c8a412ccadc190e0cafafc29774aa79ee
Jake Butler <tjakebutler@gmail.com>
2020-12-11 11:16:13 -0600

    created logic inside of ExerciseLogList that whne map function is iterating over the array of execise logs, every singleExerciseLog that is being passed in if the singleExerciseLog.userId === to the currentlyLoggedInuser which value is the localStorage Id, then it will invoke the ExerciseLog function which is imported through ExerciseLogProvider.js and only pass in the ExercsieLogObj that have a matching userId to the currently logged in localStorage Id.

--- src/components/exerciseLog/ExerciseLogList.js
+++ src/components/exerciseLog/ExerciseLogList.js
@@ -3,9 +3,13 @@ import { ExerciseLog } from "./ExerciseLog"
 import { ExerciseLogContext } from "./ExerciseLogProvider"
 
 export const ExerciseLogList = () => {
-  // This state changes when `getExerciseType()` is invoked below
+  // This state changes when `getExerciseLogs()` is invoked below
   const { exerciseLogs, addExerciseLog, getExerciseLogs } = useContext(ExerciseLogContext)
+  // currentlyLoggedInuser is setting a variable that holds the value of the currenlty logged in user held in localStorage. // 
 
+  const currentlyLoggedInuser = parseInt(localStorage.getItem("app_user_id"))
+
+// This useEffect is only being ran on render of the list and not again.
   useEffect(
     () => {
       console.log("ExerciseLogList: Initial render before data")
@@ -14,11 +18,17 @@ export const ExerciseLogList = () => {
     []
   )
 
+// The map function is iterating over the array of exercise logs, every singleExerciseLog that is being passed in if the singleExerciseLog.userId === to the currentlyLoggedInuser which value is the session storage id then it will invoke the ExerciseLog function imported through ExerciseLogContext and will only pass in the ExerciseLogObj that have a matching userId to the currenlty logged in session storageId.
+
   return (
-    <div className="">
+
+    <>
       {
-        exerciseLogs.map(singeExerciseLog => <ExerciseLog key={singeExerciseLog.id} ExerciseLogObj={singeExerciseLog} />)
-      }
-    </div>
+        exerciseLogs.map(singleExerciseLog => {
+          if (singleExerciseLog.userId === currentlyLoggedInuser)
+
+            return <ExerciseLog key={singleExerciseLog.id} ExerciseLogObj={singleExerciseLog} />
+        })}
+    </>
   )
 }
