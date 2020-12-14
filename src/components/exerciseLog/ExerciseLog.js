25d04d4d0ca70bb5390bbb0d6e8ec2e91e999cfe
Jake Butler <tjakebutler@gmail.com>
2020-12-14 15:38:18 -0600

    took out all console logs and removed all imports not being used.

--- src/components/exerciseLog/ExerciseLog.js
+++ src/components/exerciseLog/ExerciseLog.js
@@ -1,26 +1,26 @@
-import React, {useContext} from "react"
-import {ExerciseLogContext} from "./ExerciseLogProvider"
+import React, { useContext } from "react"
+import { ExerciseLogContext } from "./ExerciseLogProvider"
 
 
 export const ExerciseLog = ({ ExerciseLogObj }) => {
-  const {deleteExercise} = useContext(ExerciseLogContext)
-   
+  const { deleteExercise } = useContext(ExerciseLogContext)
+
   return <>
-  <section>
-    <h3>Exercise Log ID:{ExerciseLogObj.id}</h3>
-<div>UserId:{ExerciseLogObj.userId}</div>
-<div>ExerciseType:{ExerciseLogObj.exerciseType}</div>
-<div>Sets:{ExerciseLogObj.sets}</div>
-<div>Weight:{ExerciseLogObj.weight}</div>
-<div>Effort: {ExerciseLogObj.effort}</div>
-<div>Minutes:{ExerciseLogObj.workoutTime}</div>
-<div>Date:{ExerciseLogObj.date}</div>
-<button
+    <section>
+      <h3>Exercise Log ID:{ExerciseLogObj.id}</h3>
+      <div>UserId:{ExerciseLogObj.userId}</div>
+      <div>ExerciseType:{ExerciseLogObj.exerciseType}</div>
+      <div>Sets:{ExerciseLogObj.sets}</div>
+      <div>Weight:{ExerciseLogObj.weight}</div>
+      <div>Effort: {ExerciseLogObj.effort}</div>
+      <div>Minutes:{ExerciseLogObj.workoutTime}</div>
+      <div>Date:{ExerciseLogObj.date}</div>
+      <button
         onClick={() => {
-            deleteExercise(ExerciseLogObj.id)
-            
+          deleteExercise(ExerciseLogObj.id)
+
         }}
->Delete Exercise</button>
-  </section>
+      >Delete Exercise</button>
+    </section>
   </>
-      }
+}
