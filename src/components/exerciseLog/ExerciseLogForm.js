edecaa2ad77818ca2aac50e089d6cdd89b7f39f1
Jake Butler <tjakebutler@gmail.com>
2020-12-17 11:23:45 -0600

    now the edit button brings the users data into the log form and populates the data but the fields are not able to change

--- src/components/exerciseLog/ExerciseLogForm.js
+++ src/components/exerciseLog/ExerciseLogForm.js
@@ -10,39 +10,26 @@ export const ExerciseLogForm = (props) => {
     const { exerciseLogs, addExerciseLog, getExerciseLogs, deleteExercise, updateExerciseLog} = useContext(ExerciseLogContext)
     const { effort, getEffort } = useContext(EffortContext)
     const { exerciseTypes, getExerciseTypes } = useContext(ExerciseTypeContext)
+    
     // Component state
-    const [editLogObj, setEditLogObj] = useState({})
+    const [exerciseLogObj, setExerciseLogObj] = useState({})
 
 
-    console.log(props.location.data)
+    console.log("**Object passed via props**", props.location.data)
+    console.log("**Props**", props)
+    
     // Is there a a URL parameter??
     const editMode = props.match.params.hasOwnProperty("exerciseLog")
 
-    const handleControlledInputChange = (event) => {
-        /*
-            When changing a state object or array, always create a new one
-            and change state instead of modifying current one
-        */
-        console.log("********handleControlledInputChange Executes***********")
-        console.log(event.target)
-        console.log("current state variable editLogObj", editLogObj)
-    
-        const newEditLogObj = Object.assign({}, editLogObj)
-        console.log("new object that's a copy of editLogObj state variable", newEditLogObj)
-    
-        newEditLogObj[event.target.name] = event.target.value
-        console.log("newEditLogObj after modification", newEditLogObj)
-    
-        setEditLogObj(newEditLogObj)
-      }
 
-      const getLogInEditMode = () => {
+    const getLogInEditMode = () => {
         if (editMode) {
-          const exerciseLogId = parseInt(props.match.params.id)
-          const selectedLog = exerciseLogs.find(el => el.id === exerciseLogId) || {}
-          setEditLogObj(selectedLog)
+          const exerciseLogId = parseInt(props.match.params.exerciseLog)
+          const selectedLog = exerciseLogs.find(e => e.id === exerciseLogId) || {}
+          setExerciseLogObj(selectedLog)
         }
       }
+   
 
     // Get data from API when component initializes
     useEffect(() => {
@@ -52,6 +39,37 @@ export const ExerciseLogForm = (props) => {
        
     }, [])
 
+     // Once provider state is updated, determine the animal (if edit)
+  useEffect(() => {
+    getLogInEditMode()
+  }, [exerciseLogs])
+
+console.log(exerciseLogObj)
+
+// const constructExercise = () => {
+    
+//       if (editMode) {
+//         updateExerciseLog({
+//           id: animal.id,
+//           name: animal.name,
+//           breed: animal.breed,
+//           locationId: locationId,
+//           treatment: animal.treatment,
+//           customerId: parseInt(localStorage.getItem("kennel_customer"))
+//         })
+//           .then(() => props.history.push("/animals"))
+//       } else {
+//         addExerciseLog({
+//           name: animal.name,
+//           breed: animal.breed,
+//           locationId: locationId,
+//           treatment: animal.treatment,
+//           customerId: parseInt(localStorage.getItem("kennel_customer"))
+//         })
+//           .then(() => props.history.push("/animals"))
+//       }
+//     }
+//   }
 
 
     const { register, handleSubmit } = useForm();
@@ -69,12 +87,15 @@ export const ExerciseLogForm = (props) => {
         
     }
 
+    
+
+
     return (
         <>
             <h1>Log your new exercise!</h1>
             <form  onSubmit={handleSubmit(onSubmit)}>
                 <div className="form-group">
-                <select  name="exerciseTypeId" placeholder="" ref={register}>
+                <select  name="exerciseTypeId" placeholder="" ref={register} value={exerciseLogObj.exerciseTypeId}> 
 
                     <option value="0">---Select Your Exercise Type---</option>
                     {exerciseTypes.map(exerciseTypes => {
@@ -91,13 +112,14 @@ export const ExerciseLogForm = (props) => {
                 </select>
                 </div>
                 <div className="form-group">    
-                <input className="form-control form-control-lg" type="text" placeholder="Sets" name="sets" ref={register} />
+                <input className="form-control form-control-lg" type="text" placeholder="Sets" name="sets" ref={register}  value={exerciseLogObj.sets}/>
+                </div>
+
+                <div className="form-group">
+                <input class="form-control form-control-lg" type="text" placeholder="Weight" name="weight" ref={register} value={exerciseLogObj.weight}/>
                 </div>
                 <div className="form-group">
-                <input class="form-control form-control-lg" type="text" placeholder="Weight" name="weight" ref={register} />
-                </div>
-                <div className="form-group">
-                <select name="effortId" placeholder="" ref={register}>
+                <select name="effortId" placeholder="" ref={register} value={exerciseLogObj.effortId} >
 
                     <option value="0">---Your effort on a scale 1-10---</option>
                     {effort.map(effort => (<option key={effort.id} value={effort.id}>
@@ -106,10 +128,10 @@ export const ExerciseLogForm = (props) => {
                 </select>
                 </div>        
                 <div className="form-group">
-                <input className="form-control form-control-lg" type="text" placeholder="Attitude" name="attitude" ref={register} />
+                <input className="form-control form-control-lg" type="text" placeholder="Attitude" name="attitude" ref={register} value={exerciseLogObj.attitude}/>
                 </div>
                 <div className="form-group">
-                <input className="form-control form-control-lg" type="text" placeholder="Time" name="workoutTime" ref={register} />
+                <input className="form-control form-control-lg" type="text" placeholder="Time" name="workoutTime" ref={register} value={exerciseLogObj.workoutTime} />
                 </div>
                 <input  type="hidden" value={new Date().toLocaleDateString('en-US')} name="date" ref={register} />
 
